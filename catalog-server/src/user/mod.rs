ca58f14423ec70b7689c92f31a87cf3ef0e23be5
Domenico Cerasuolo <cerasuolodomenico@gmail.com>
2025-11-24 22:05:59 +0100

    [catalog-server] Update permission and roles

--- catalog-server/src/user/mod.rs
+++ catalog-server/src/user/mod.rs
@@ -1,11 +1,12 @@
 pub mod auth;
-pub(self) mod permissions;
+pub mod permissions;
 mod sqlite_user_store;
 mod user_manager;
 pub mod user_models;
 mod user_store;
 
 pub use auth::{AuthToken, AuthTokenValue, UserAuthCredentials, UsernamePasswordCredentials};
+pub use permissions::{Permission, PermissionGrant, UserRole};
 pub use sqlite_user_store::SqliteUserStore;
 pub use user_manager::UserManager;
 pub use user_models::{LikedContentType, User, UserPlaylist, UserSessionView};
--- catalog-server/src/user/permissions.rs
+++ catalog-server/src/user/permissions.rs
@@ -1,5 +1,6 @@
 use std::time::SystemTime;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Permission {
     AccessCatalog,
     LikeContent,
@@ -9,7 +10,38 @@ pub enum Permission {
     IssueContentDownload,
     RebootServer,
 }
+
+impl Permission {
+    pub fn to_int(&self) -> i32 {
+        match self {
+            Permission::AccessCatalog => 1,
+            Permission::LikeContent => 2,
+            Permission::OwnPlaylists => 3,
+            Permission::EditCatalog => 4,
+            Permission::ManagePermissions => 5,
+            Permission::IssueContentDownload => 6,
+            Permission::RebootServer => 7,
+        }
+    }
+
+    pub fn from_int(value: i32) -> Option<Self> {
+        match value {
+            1 => Some(Permission::AccessCatalog),
+            2 => Some(Permission::LikeContent),
+            3 => Some(Permission::OwnPlaylists),
+            4 => Some(Permission::EditCatalog),
+            5 => Some(Permission::ManagePermissions),
+            6 => Some(Permission::IssueContentDownload),
+            7 => Some(Permission::RebootServer),
+            _ => None,
+        }
+    }
+}
+
 const ADMIN_PERMISSIONS: &'static [Permission] = &[
+    Permission::AccessCatalog,
+    Permission::LikeContent,
+    Permission::OwnPlaylists,
     Permission::EditCatalog,
     Permission::ManagePermissions,
     Permission::IssueContentDownload,
@@ -21,27 +53,43 @@ const REGULAR_PERMISSIONS: &'static [Permission] = &[
     Permission::OwnPlaylists,
 ];
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum UserRole {
     Admin,
     Regular,
 }
 
 impl UserRole {
-    fn permissions(&self) -> &'static [Permission] {
+    pub fn permissions(&self) -> &'static [Permission] {
         match self {
             UserRole::Admin => ADMIN_PERMISSIONS,
             UserRole::Regular => REGULAR_PERMISSIONS,
         }
     }
+
+    pub fn to_string(&self) -> &'static str {
+        match self {
+            UserRole::Admin => "Admin",
+            UserRole::Regular => "Regular",
+        }
+    }
+
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "Admin" => Some(UserRole::Admin),
+            "Regular" => Some(UserRole::Regular),
+            _ => None,
+        }
+    }
 }
 
+#[derive(Debug, Clone)]
 pub enum PermissionGrant {
     ByRole(UserRole),
-    Extra(Permission),
-    OneOff(Permission),
-    Timed {
+    Extra {
         start_time: SystemTime,
-        end_time: SystemTime,
-        permissions: Vec<Permission>,
+        end_time: Option<SystemTime>,
+        permission: Permission,
+        countdown: Option<u64>,
     },
 }
