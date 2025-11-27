3bdfc5b8f848d7053d405d593b0f950bad7d2199
Domenico Cerasuolo <cerasuolodomenico@gmail.com>
2025-11-27 21:59:35 +0100

    [android] Update play/pause button to use theme-aware contrast color
    - PlayerScreen: Change play/pause icon tint to MaterialTheme.colorScheme.onSurface
    - MainScreen: Use simple play/pause icons and onSurface tint for theme adaptation
    - Respects app's custom theme mode (System/Light/Dark) setting

--- android/ui/src/main/java/com/lelloman/pezzottify/android/ui/screen/player/PlayerScreen.kt
+++ android/ui/src/main/java/com/lelloman/pezzottify/android/ui/screen/player/PlayerScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -45,7 +44,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
@@ -440,7 +438,7 @@ private fun PlaybackControls(
                 ),
                 contentDescription = if (isPlaying) "Pause" else "Play",
                 modifier = Modifier.size(40.dp),
-                tint = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                tint = MaterialTheme.colorScheme.onSurface,
             )
         }
 
