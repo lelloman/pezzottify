b5238994b67b71c5a9fff2045cadbb3f6fd1a219
Domenico Cerasuolo <cerasuolodomenico@gmail.com>
2025-12-08 18:35:40 +0100

    [background_jobs] Add PopularContentJob implementation
    Add PopularContentJob that pre-computes popular albums and artists based on
    listening data. The job:
    - Runs on startup and every 6 hours
    - Aggregates listening stats to find top content
    - Warms catalog caches by resolving album/artist JSON
    - Supports cancellation during execution
    
    Register the job in main.rs scheduler initialization.

--- catalog-server/src/background_jobs/jobs/mod.rs
+++ catalog-server/src/background_jobs/jobs/mod.rs
@@ -3,6 +3,10 @@
 //! This module contains implementations of the `BackgroundJob` trait
 //! for various server maintenance and processing tasks.
 
+pub mod popular_content;
+
+pub use popular_content::PopularContentJob;
+
 // Future job implementations will be added here:
 // pub mod event_pruning;
 // pub mod search_reindex;
