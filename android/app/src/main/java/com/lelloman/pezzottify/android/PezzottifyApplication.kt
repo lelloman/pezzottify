5f778e0fd1b34ba89f9abad0c3ad31e31c88c7e9
Domenico Cerasuolo <cerasuolodomenico@gmail.com>
2025-12-09 15:56:29 +0100

    [android] Add ExoPlayer error logging and global exception handler
    - Add Logger to ExoPlatformPlayer and implement onPlayerError callback
    - Remove debug Log.d calls from player module
    - Add UncaughtExceptionHandler in PezzottifyApplication to capture
      crashes in file logs before the app terminates

--- android/app/src/main/java/com/lelloman/pezzottify/android/PezzottifyApplication.kt
+++ android/app/src/main/java/com/lelloman/pezzottify/android/PezzottifyApplication.kt
@@ -2,6 +2,7 @@ package com.lelloman.pezzottify.android
 
 import android.app.Application
 import com.lelloman.pezzottify.android.domain.usecase.InitializeApp
+import com.lelloman.pezzottify.android.logger.LoggerFactory
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -11,8 +12,27 @@ class PezzottifyApplication : Application() {
     @Inject
     lateinit var initializeApp: InitializeApp
 
+    @Inject
+    lateinit var loggerFactory: LoggerFactory
+
     override fun onCreate() {
         super.onCreate()
+        setupUncaughtExceptionHandler()
         initializeApp()
+    }
+
+    private fun setupUncaughtExceptionHandler() {
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+        val logger = loggerFactory.getLogger("UncaughtException")
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            try {
+                logger.error("Uncaught exception in thread '${thread.name}'", throwable)
+            } catch (_: Exception) {
+                // Ignore any logging failures - we don't want to cause another crash
+            }
+            // Delegate to default handler so the app crashes normally
+            defaultHandler?.uncaughtException(thread, throwable)
+        }
     }
 }
