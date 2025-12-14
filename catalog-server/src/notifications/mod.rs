b9b141d81722efff2f984edd1870c7bbe87a4f79
Domenico Cerasuolo <cerasuolodomenico@gmail.com>
2025-12-14 09:56:53 +0100

    [catalog-server] Add NotificationService
    Create NotificationService that handles:
    - Creating notifications in the database
    - Logging NotificationCreated sync events
    - Broadcasting notifications to connected WebSocket clients

--- catalog-server/src/notifications/mod.rs
+++ catalog-server/src/notifications/mod.rs
@@ -1,7 +1,9 @@
 //! User notifications module
 
 mod models;
+mod service;
 mod store;
 
 pub use models::{DownloadCompletedData, Notification, NotificationType};
+pub use service::NotificationService;
 pub use store::NotificationStore;
