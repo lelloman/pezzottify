a30731b7322331a4150827600fd4147418c70a14
Domenico Cerasuolo <cerasuolodomenico@gmail.com>
2026-05-24 13:21:14 +0200

    Clarify smart continuation control

---
+++ web/src/components/icons/AiContinuationIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg viewBox="0 0 24 24" aria-hidden="true">
+    <path d="M8 3.5L10.6 6L8 8.5L5.4 6L8 3.5Z" />
+    <path d="M14.7 5.2L17 7.4L14.7 9.6L12.4 7.4L14.7 5.2Z" />
+    <path d="M8.9 11.5L11 13.5L8.9 15.5L6.8 13.5L8.9 11.5Z" />
+    <path d="M17 13V16H20V18H17V21H15V18H12V16H15V13H17Z" fill-rule="evenodd" />
+  </svg>
+</template>
